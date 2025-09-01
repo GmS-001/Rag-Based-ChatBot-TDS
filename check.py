f8669efbe44127c2163f555c7e00323e6a63c6d9
Garvit Man Singh <garvitmansingh830@gmail.com>
2025-09-02 00:36:33 +0530

    Removed unnecessay files.

--- check.py
+++
@@ -1,9 +0,0 @@
-from langchain_google_genai import GoogleGenerativeAI
-from dotenv import load_dotenv
-
-load_dotenv()
-model = GoogleGenerativeAI(model = 'gemini-2.5-flash')
-result = model.invoke("What is the capital of itlay")
-print(type(result))
-print(len(result))
-print(result)
